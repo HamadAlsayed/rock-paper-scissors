ee4dc4c8c26916dba69b49e5cf89e39a873352aa
HamadAlsayed <hamad_sayed@outlook.com>
2023-08-04 21:58:09 +0300

    Add function getRandomNumber()
    this returns a random number between two given values

---
+++ script.js
@@ -0,0 +1,11 @@
+function getRandomNumber (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getComputerChoice () {
+    
+}
+
+console.log(getRandomNumber(1, 3));
