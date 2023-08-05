80af18cf07388bd149a6c4aa39113034b955ad22
HamadAlsayed <hamad_sayed@outlook.com>
2023-08-05 13:36:19 +0300

    Change result text

--- script.js
+++ script.js
@@ -33,18 +33,19 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         return "It's a tie";
     } else if (playerChoice === 1 && computerChoice === 2) {
-        return "Computer wins!";
+        return "You lose! Paper beats rock.";
     } else if (playerChoice === 1 && computerChoice === 3) {
-        return "Player wins!";
+        return "You win! Rock beats scissors.";
     } else if (playerChoice === 2 && computerChoice === 1) {
-        return "Player wins!";
+        return "You win! Paper beats rock.";
     } else if (playerChoice === 2 && computerChoice === 3) {
-        return "Computer wins!";
+        return "You lose! Scissors beat paper.";
     } else if (playerChoice === 3 && computerChoice === 1) {
-        return "Computer wins!";
+        return "You lose! Rock beats scissors.";
     } else if (playerChoice === 3 && computerChoice === 2) {
-        return "Player wins!";
+        return "You win! Scissors beat paper.";
     }
+
 }
 
 const playerChoice = getPlayerChoice();
