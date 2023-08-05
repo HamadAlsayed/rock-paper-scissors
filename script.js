e81534ee5cb203c666a7761b28a0e20c86693f76
HamadAlsayed <hamad_sayed@outlook.com>
2023-08-05 14:02:32 +0300

    Fix getComputerChoice() output
    this previously used to return string values instead of the required integer values for playRound()

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
-function getRandomNumber (min, max) {
+let playerScore = 0;
+let computerScore = 0;
+
+function getRandomNumber(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function getComputerChoice () {
+function getComputerChoice() {
     let choice = getRandomNumber(1, 3);
     
-    if (choice === 1) {
-        return "Rock";
-    } else if (choice === 2) {
-        return "Paper";
-    } else {
-        return "Scissors";
-    }
-}
-
-function getPlayerChoice () {
-    let input = prompt("Pick between rock, paper or scissors.", "Please type your choice here.").toLowerCase();
-
-    if (input === "rock") {
+    if (choice == 1) {
         return 1;
-    } else if (input === "paper") {
+    } else if (choice == 2) {
         return 2;
     } else {
         return 3;
     }
 }
 
+function getPlayerChoice() {
+    let playerInput = prompt("Pick between rock, paper or scissors.", "Please type your choice here.");
+
+    if (playerInput == null) {
+        alert("Please enter a valid choice.");
+    } else {
+        playerInput = playerInput.toLowerCase();
+        if (playerInput == "rock") {
+            return 1;
+        } else if (playerInput == "paper") {
+            return 2;
+        } else if (playerInput == "scissors"){
+            return 3;
+        } else {
+            return 1;
+        }
+    }
+    
+}
+
 function playRound(playerChoice, computerChoice) {
 
-    if (playerChoice === computerChoice) {
+
+
+    if (playerChoice == computerChoice) {
         return "It's a tie";
-    } else if (playerChoice === 1 && computerChoice === 2) {
+    } else if (playerChoice == 1 && computerChoice == 2) {
         return "You lose! Paper beats rock.";
-    } else if (playerChoice === 1 && computerChoice === 3) {
+    } else if (playerChoice == 1 && computerChoice == 3) {
         return "You win! Rock beats scissors.";
-    } else if (playerChoice === 2 && computerChoice === 1) {
+    } else if (playerChoice == 2 && computerChoice == 1) {
         return "You win! Paper beats rock.";
-    } else if (playerChoice === 2 && computerChoice === 3) {
+    } else if (playerChoice == 2 && computerChoice == 3) {
         return "You lose! Scissors beat paper.";
-    } else if (playerChoice === 3 && computerChoice === 1) {
+    } else if (playerChoice == 3 && computerChoice == 1) {
         return "You lose! Rock beats scissors.";
-    } else if (playerChoice === 3 && computerChoice === 2) {
+    } else if (playerChoice == 3 && computerChoice == 2) {
         return "You win! Scissors beat paper.";
     }
 
 }
 
-const playerChoice = getPlayerChoice();
-const computerChoice = getComputerChoice();
-console.log(playRound(playerChoice, computerChoice));
+function game() {
+    if (playerScore != 5 || computerScore != 5) {
+        const playerChoice = getPlayerChoice();
+        const computerChoice = getComputerChoice();
+        console.log(playRound(playerChoice, computerChoice));
+    }
+
+}
+
+game();
